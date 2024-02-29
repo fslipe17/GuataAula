df4525ca5ef15b511e532225f172bf25ea3ac4f7
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2024-02-29 09:53:57 -0300

    Add files via upload

---
+++ j.py
@@ -0,0 +1,26 @@
+#N1 = input('digite seu nome:')
+#N2 = input('digite seu sobrenome:')
+
+#print('seu nome é', N1,'e seu sobrenome é', N2)
+
+##################################################
+
+#n = int(input('Digite um Numero:'))
+
+#if n > 10:
+#    print("O numero é maior que 10.")
+#elif n == 10:
+#    print("O número é exatamente 10.")
+#else:
+#    print("O numero é menor que 10.")
+
+########################################################
+
+N = int(input('Digite sua idade:'))
+
+if N <= 17:
+    print('Você é Adolecente!!')
+elif N < 13:
+    print('Você é Criança!!')
+else:
+    print('Você é Adulto!!')

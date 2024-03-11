9cf2d154f7ff8426e4247e66a5d9bea6eb9020f4
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2024-03-11 09:39:59 -0300

    Add files via upload

--- pietro.py
+++ pietro.py
@@ -1,1 +1,37 @@
-print ('ola joao')
+print("================================")
+print("Bem-vindo ao jogo de Adivinhação")
+print("================================")
+
+
+import random
+
+numero = random.randint(0, 100)
+
+print("")
+
+tentativas = 10
+rodada = 1
+
+while (rodada <= tentativas):
+    print("Tentativa {} de 10".format(rodada, tentativas))
+
+    chute_str = input("Digite o seu numero: ")
+    print("Você Digitou", chute_str)
+    chute = int(chute_str)
+
+    acertou = chute == numero
+    maior = chute < numero
+    menor = chute > numero
+
+    if(acertou):
+        print("Parabéns!! Você acertou ;)")
+        rodada = rodada + 11111111111111111111111111111111111111111111111111
+    else:
+        if(maior):
+            print("O número é maior que", chute)
+        elif(menor):
+            print("O numero é menor que", chute)
+
+    rodada = rodada + 1
+
+print("Fim de jogo")
